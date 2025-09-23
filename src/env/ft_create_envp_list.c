e08218d39bfef2f069c110c691231c7a5aac194e
Gautier Rivoire <garivoir@f6r10s16.clusters.42paris.fr>
2025-09-23 15:29:45 +0200

    built_in envp creation malloc correction

--- src/env/ft_create_envp_list.c
+++ src/env/ft_create_envp_list.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_create_envp_list.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: echatela <echatela@student.42.fr>          +#+  +:+       +#+        */
+/*   By: garivoir <garivoir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/16 16:32:55 by garivoir          #+#    #+#             */
-/*   Updated: 2025/09/23 11:57:35 by echatela         ###   ########.fr       */
+/*   Updated: 2025/09/23 15:22:48 by garivoir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,28 +42,28 @@ void	ft_clear_lstenvp(t_env **envp, int status)
 /*------------------------------*/
 t_ret	ft_create_lstenvp(t_ms *ms, char **old_envp)
 {
+	int		i;
 	t_env	*new_envp;
 	t_env	*new_envp_temp;
 	t_env	*new_envp_prev_temp;
 
-	int (i) = 0;
 	if (!old_envp || !old_envp[0])
 		return (MS_ERR);
 	new_envp = ft_new_envp(old_envp[0]);
 	if (!new_envp)
 		return (ms_fatal(ms, "malloc"), MS_ERR);
+	i = 0;
 	new_envp_temp = new_envp;
-	while (old_envp[++i])
+	while (old_envp[i])
 	{
 		new_envp_prev_temp = new_envp;
 		new_envp->next = ft_new_envp(old_envp[i]);
 		if (!new_envp->next)
-		{
-			ft_clear_lstenvp(&new_envp_temp, -1);	//error
-			return (ms_fatal(ms, "malloc"), MS_ERR);
-		}
+			return (ft_clear_lstenvp(&new_envp_temp, -1),	//error
+					ms_fatal(ms, "malloc"), MS_ERR);
 		new_envp = new_envp->next;
 		new_envp->prev = new_envp_prev_temp;
+		i++;
 	}
 	ms->env = new_envp_temp;
 	return (MS_OK);
