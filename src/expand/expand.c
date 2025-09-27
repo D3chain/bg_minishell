ef41f2a0b946ae32a5b800fffcc8d151f37f2965
D3chain <edchatelain9@gmail.com>
2025-09-27 15:07:04 +0200

    start expand

--- src/expand/expand.c
+++ src/expand/expand.c
@@ -6,10 +6,22 @@
 /*   By: echatela <echatela@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/27 12:44:03 by echatela          #+#    #+#             */
-/*   Updated: 2025/09/27 12:52:52 by echatela         ###   ########.fr       */
+/*   Updated: 2025/09/27 15:06:38 by echatela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-t_cmd	expand()
+
+
+void	expand(t_ms *ms, t_cmd *cmd)
+{
+	int	i;
+
+	i = 0;
+	while (cmd->argv.data[i])
+	{
+		
+	}
+	expand_redv(ms, &cmd->redv);
+}
